8132e40d78cb3ed5bb01e33aa7814b6e92f80402
msi-ism <msism720@gmail.com>
2022-09-11 23:14:56 -0400

    Yell at the Ninja Turtles answered

--- module_1/javascript_practice/exercises/arr-con-loop-hw.js
+++ module_1/javascript_practice/exercises/arr-con-loop-hw.js
@@ -33,30 +33,33 @@
 ////////////////////////////////
 // ** Wild Wild Life
 ////////////////////////////////
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-// 1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
-plantee[2]++
-console.log(plantee[2])
-// 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
-wolfy[3] = 'Gotham City'
-console.log(wolfy)
-// 3. Give D'Art a second hometown by adding "Hawkins"
-dart.push('Hawkins')
-console.log(dart)
-// 4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
-wolfy.shift()
-wolfy.unshift('Gameboy')
-console.log(wolfy)
+// // 1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// plantee[2]++
+// console.log(plantee[2])
+// // 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// wolfy[3] = 'Gotham City'
+// console.log(wolfy)
+// // 3. Give D'Art a second hometown by adding "Hawkins"
+// dart.push('Hawkins')
+// console.log(dart)
+// // 4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+// wolfy.shift()
+// wolfy.unshift('Gameboy')
+// console.log(wolfy)
 ////////////////////////////////
 // ** Yell at the Ninja Turtles
 ////////////////////////////////
+ninjaTurts = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
 
-
+for (turt of ninjaTurts) {
+    console.log(turt.toUpperCase())
+}
 ////////////////////////////////
 // ** Methods, Revisited
 ////////////////////////////////
