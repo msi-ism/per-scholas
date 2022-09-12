71ff927a7a543a6d3c2b992c1c2775ffb8a83866
msi-ism <msism720@gmail.com>
2022-09-11 23:06:55 -0400

    Wild Wild Life answered

--- module_1/javascript_practice/exercises/arr-con-loop-hw.js
+++ module_1/javascript_practice/exercises/arr-con-loop-hw.js
@@ -18,25 +18,40 @@
 ////////////////////////////////
 // ** Fizz Buzz
 ////////////////////////////////
-for (let i = 1; i < 101; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("Fizzbuzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-} 
-
-
+// for (let i = 1; i < 101; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("Fizzbuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// } 
 
 ////////////////////////////////
 // ** Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-
+// 1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2]++
+console.log(plantee[2])
+// 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = 'Gotham City'
+console.log(wolfy)
+// 3. Give D'Art a second hometown by adding "Hawkins"
+dart.push('Hawkins')
+console.log(dart)
+// 4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+wolfy.shift()
+wolfy.unshift('Gameboy')
+console.log(wolfy)
 ////////////////////////////////
 // ** Yell at the Ninja Turtles
 ////////////////////////////////
