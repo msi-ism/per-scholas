8b3746256bc6d7886fe7093d7ffe43c0d818dcc7
msi-ism <msism720@gmail.com>
2022-09-18 20:44:26 -0400

    added easy pattern

--- module_1/projects/simon-game/script.js
+++ module_1/projects/simon-game/script.js
@@ -15,12 +15,12 @@ let pad9 = document.getElementById('padNine')
 const padArr = [pad1, pad2, pad3, pad4, pad5, pad6, pad7, pad8, pad9]
 
 // ** Pad Light On & Off Variable
-let pad1Light = '0px 0px 40px 20px rgba(253, 245, 232, 0.8), inset 0 0 25px 25px rgba(253, 245, 232, 0.3)'
+let padLight = '0px 0px 40px 20px rgba(253, 245, 232, 0.8), inset 0 0 25px 25px rgba(253, 245, 232, 0.3)'
 let lightOff = ''
 
 // ** Function to Light Individual Pads
 const lightPad = (pad) => {
-    pad.style.boxShadow = pad1Light
+    pad.style.boxShadow = padLight
 }
 // ** Function to UnLight Individual Pads
 const offPad = (pad) => {
@@ -77,55 +77,6 @@ const blinkAll = () => {
     setTimeout(function () { offPad(pad9); }, 200)
 }
 
-// ** Calling StartupLights & blinkTwice to Initialize Game
-
-const startUpLights = () => {
-    lightEmUp()
-    setTimeout(lightEmDown, 1000)
-    
-}
-startUpLights()
-
-const blinkTwice = () => {
-    setTimeout(blinkAll, 1500)
-    setTimeout(blinkAll, 1750)
-}
-
-blinkTwice()
-
-const padTap = (pad) => {
-    pad.style.boxShadow = pad1Light
-    setTimeout(function () { offPad(pad); }, 250)
-
-}
-
-
-// ** Adds Tap Lighting Effect to Individual Pads
-padArr.forEach(pad => { pad.addEventListener('click', function() { padTap(this); }, false)})
-
-// ** Begins Function Objects
-
-class Pad {
-    constructor(name, color, tone, selector){
-        this.name = name
-        this.color = color
-        this.tone = tone
-        this.selector = selector
-    }
-    padTap(){       
-    }
-}
-
-const padObj1 = new Pad('pad-1', '#E8ECFB', 'A#', `${pad1}`)
-const padObj2 = new Pad('pad-2', '#B997C7', 'A#', `${pad2}`)
-const padObj3 = new Pad('pad-3', '#824D99', 'A#', `${pad3}`)
-const padObj4 = new Pad('pad-4', '#4D78C3', 'A#', `${pad4}`)
-const padObj5 = new Pad('pad-5', '#56A2AC', 'A#', `${pad5}`)
-const padObj6 = new Pad('pad-6', '#7EB775', 'A#', `${pad6}`)
-const padObj7 = new Pad('pad-7', '#CD2120', 'A#', `${pad7}`)
-const padObj8 = new Pad('pad-8', 'E57F33', 'A#', `${pad8}`)
-const padObj9 = new Pad('pad-9', '#D0B541', 'A#', `${pad9}`)
-
 // ** Text Animation Function
 
 
@@ -160,12 +111,81 @@ const complete =() => {
 }
 
 
+// ** Begins Function Objects
 
-pad9.style.backgroundColor = padObj9['color']
+class Pad {
+    constructor(name, color, tone, selectorVar){
+        this.name = name
+        this.color = color
+        this.tone = tone
+        this.selectorVar = selectorVar
+    }
+    padTap(){       
+    }
+}
+
+const padObj1 = new Pad('pad-1', '#E8ECFB', 'A#', pad1)
+const padObj2 = new Pad('pad-2', '#B997C7', 'B#', pad2)
+const padObj3 = new Pad('pad-3', '#824D99', 'C#', pad3)
+const padObj4 = new Pad('pad-4', '#4D78C3', 'D#', pad4)
+const padObj5 = new Pad('pad-5', '#56A2AC', 'E#', pad5)
+const padObj6 = new Pad('pad-6', '#7EB775', 'F#', pad6)
+const padObj7 = new Pad('pad-7', '#CD2120', 'G#', pad7)
+const padObj8 = new Pad('pad-8', 'E57F33', 'A#', pad8)
+const padObj9 = new Pad('pad-9', '#D0B541', 'B#', pad9)
+
+
+
+// ** Calling StartupLights & blinkTwice to Initialize Game
+
+const startUpLights = () => {
+    lightEmUp()
+    setTimeout(lightEmDown, 1000)
+    
+}
+startUpLights()
+
+const blinkTwice = () => {
+    setTimeout(blinkAll, 1500)
+    setTimeout(blinkAll, 1750)
+}
+
+blinkTwice()
+
+const padTap = (pad) => {
+    pad.style.boxShadow = padLight
+    setTimeout(function () { offPad(pad); }, 250)
+
+}
+
+
+// ** Adds Tap Lighting Effect to Individual Pads
+padArr.forEach(pad => { pad.addEventListener('click', function() { padTap(this); }, false)})
+
+
 
 // ** Game Logic Begins
+let easyPatArr = []
 
 
+for (let i = 0; i < 5; i++) {
+    let randomPad = Math.floor(Math.random() * padArr.length)
+    easyPatArr.push(padArr[randomPad])
+
+}
+
+
+let patTest = () => {
+        setTimeout(function () { padTap(easyPatArr[0]); }, 500)
+        setTimeout(function () { padTap(easyPatArr[1]); }, 1000)
+        setTimeout(function () { padTap(easyPatArr[2]); }, 1500)
+        setTimeout(function () { padTap(easyPatArr[3]); }, 2000)
+        setTimeout(function () { padTap(easyPatArr[4]); }, 2500)
+
+}
+
+const startBtn = document.querySelector('#start')
+startBtn.addEventListener('click', patTest)
 
 
 
