fb8b8a9a55e382198d75df59543ce399fe4095e5
msi-ism <msism720@gmail.com>
2022-09-30 10:21:02 -0400

    Magic 8 Ball express application created.

--- module-2/express-homework/express-hw-1/server.js
+++ module-2/express-homework/express-hw-1/server.js
@@ -29,12 +29,18 @@ const express = require('express')
  })
 
 
+ const answerArr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 
 
+ // ^ Magic 8 Ball Route
+ app.get('/magic/:question/', function(req, res){
+   const answer = answerArr[Math.floor(Math.random() * answerArr.length)]
+   const {question} = req.params
+   res.send(`<h1>The answer to your question: ${question} is: ${answer}!</h1>`)
+})
 
-
-
+// http://localhost:3000/magic/Will%20I%20Ever%20Fly%3F
 
 // ^ Declaring port
  app.listen(PORT, () => {
